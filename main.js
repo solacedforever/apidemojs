030f8c7d9c78ae4f0b2142688e805ba38004618c
James Spargo <solacedforever@gmail.com>
2017-07-19 12:29:53 -0400

    indent changes

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-// //
-//
-//
 let button = document.querySelector(".button");
 let recipe = document.querySelector(".container");
 let rec = document.querySelector("#input");
 
-button.addEventListener("click", function searchRecipes () {
+button.addEventListener("click", function searchRecipes() {
 
-  fetch("https://crossorigin.me/http://www.recipepuppy.com/api/?i="+ rec.value)
+  fetch("https://crossorigin.me/http://www.recipepuppy.com/api/?i=" + rec.value)
     // Data is fetched and we get a promise.
     .then(
       // The promise returns a response from the server.
@@ -19,30 +16,21 @@ button.addEventListener("click", function searchRecipes () {
           return;
         }
         response.json().then(function(response) {
-          console.log("results is:", response);
-          let template = "";
-          response.results.forEach(function(data) {
-            template += `
+            console.log("results is:", response);
+            let template = "";
+            response.results.forEach(function(data) {
+              template += `
            <div recipe class="recipe">
               <img src="${data.thumbnail}"
             <h3> <a href="${data.href}"
              target"blank">${data.title}</a></h3></div>`;
-             recipe.innerHTML = template;
-
-
-
-          // let data = JSON.parse(this.responseText);
-
-        });
+              recipe.innerHTML = template;
+            });
+          })
+            .catch(function(err) {
+              console.log("Fetch Error :-S", err);
+          })
       }
-    )
+    );
 
-
-    .catch(function(err) {
-      console.log("Fetch Error :-S", err);
-    })
-    }
-  );
-
-}
-)
+})
